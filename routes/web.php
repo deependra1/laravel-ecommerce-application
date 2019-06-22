f401b7fb80754bf28cea9d480a88d1f9d63b55c5
Deependra Adhikari <rythm.dipen@gmail.com>
2019-06-22 15:20:24 +0545

    Implementing website setting

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-require 'admin.php';
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/admin', 'admin.dashboard.index');
-Route::view('/admin/login', 'admin.auth.login');
+Auth::routes();
+require 'admin.php';
